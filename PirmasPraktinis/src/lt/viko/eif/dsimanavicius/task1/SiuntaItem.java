955c684fa766aac08ec94417a626cb3e0a6aa6f7
D0mazo <dominykas.simanavicius@proton.me>
2026-03-19 13:20:02 +0200

    added comments

--- PirmasPraktinis/src/lt/viko/eif/dsimanavicius/task1/SiuntaItem.java
+++ PirmasPraktinis/src/lt/viko/eif/dsimanavicius/task1/SiuntaItem.java
@@ -11,28 +11,81 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class SiuntaItem {
 
+    /** Unique package identifier. */
     private int id;
+
+    /** Description of the package contents. */
     private String description;
+
+    /** Weight of the package in kg. */
     private float weight;
 
+    /**
+     * Default constructor required by JAXB.
+     */
     public SiuntaItem() {
     }
 
+    /**
+     * Creates a package item with all fields.
+     *
+     * @param id          unique package ID
+     * @param description package contents description
+     * @param weight      package weight in kg
+     */
     public SiuntaItem(int id, String description, float weight) {
         this.id          = id;
         this.description = description;
         this.weight      = weight;
     }
 
+    /**
+     * Returns the package ID.
+     *
+     * @return package ID
+     */
     public int getId() { return id; }
+
+    /**
+     * Sets the package ID.
+     *
+     * @param id package ID
+     */
     public void setId(int id) { this.id = id; }
 
+    /**
+     * Returns the package description.
+     *
+     * @return description
+     */
     public String getDescription() { return description; }
+
+    /**
+     * Sets the package description.
+     *
+     * @param description package description
+     */
     public void setDescription(String description) { this.description = description; }
 
+    /**
+     * Returns the package weight.
+     *
+     * @return weight in kg
+     */
     public float getWeight() { return weight; }
+
+    /**
+     * Sets the package weight.
+     *
+     * @param weight weight in kg
+     */
     public void setWeight(float weight) { this.weight = weight; }
 
+    /**
+     * Returns a string representation of the package item.
+     *
+     * @return formatted package string
+     */
     @Override
     public String toString() {
         return "SiuntaItem{id=" + id +
--- PirmasPraktinis/src/lt/viko/eif/dsimanavicius/task1/Siuntos.java
+++ PirmasPraktinis/src/lt/viko/eif/dsimanavicius/task1/Siuntos.java
@@ -3,8 +3,6 @@ package lt.viko.eif.dsimanavicius.task1;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
-import lt.viko.eif.dsimanavicius.task1.SiuntaItem;
-
 import java.util.List;
 
 /**
@@ -15,19 +13,44 @@ import java.util.List;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Siuntos {
 
+    /** List of package items in the shipment. */
     @XmlElement(name = "packageItem")
-    private List<lt.viko.eif.dsimanavicius.task1.SiuntaItem> packageItems;
+    private List<SiuntaItem> packageItems;
 
+    /**
+     * Default constructor required by JAXB.
+     */
     public Siuntos() {
     }
 
-    public Siuntos(List<lt.viko.eif.dsimanavicius.task1.SiuntaItem> packageItems) {
+    /**
+     * Creates a wrapper with a list of package items.
+     *
+     * @param packageItems list of package items
+     */
+    public Siuntos(List<SiuntaItem> packageItems) {
         this.packageItems = packageItems;
     }
 
-    public List<lt.viko.eif.dsimanavicius.task1.SiuntaItem> getPackageItems() { return packageItems; }
+    /**
+     * Returns the list of package items.
+     *
+     * @return list of packages
+     */
+    public List<SiuntaItem> getPackageItems() { return packageItems; }
+
+    /**
+     * Sets the list of package items.
+     *
+     * @param packageItems list of packages
+     */
     public void setPackageItems(List<SiuntaItem> packageItems) { this.packageItems = packageItems; }
 
+    /**
+     * Returns a string representation of the packages list.
+     *
+     * @return formatted string
+     */
     @Override
     public String toString() {
         return "Siuntos{packageItems=" + packageItems + '}';
